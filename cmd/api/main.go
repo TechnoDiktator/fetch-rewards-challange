911568e718c35bbeb57c39250a179d3e7c3b6e0d
tarang <tarang@stackfusion.io>
2024-11-24 03:34:58 +0530

    gracefull stop for the servcer added

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,15 +1,28 @@
 package main
 
 import (
+	"context"
 	"github.com/TechnoDiktator/fetch-rewards-challange/internal/handlers"
 	"github.com/TechnoDiktator/fetch-rewards-challange/internal/middlewares"
 	"github.com/TechnoDiktator/fetch-rewards-challange/internal/repository"
 	"github.com/TechnoDiktator/fetch-rewards-challange/internal/services"
 	"github.com/TechnoDiktator/fetch-rewards-challange/internal/utils/constants"
 	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+	"golang.org/x/net/http2"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
+
+	// Set up signal handling to gracefully shut down
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+
 	// Initialize Gin router
 	r := gin.Default()
 
@@ -30,5 +43,51 @@ func main() {
 	r.GET(constants.GetPoints, receiptHandler.GetPoints)
 
 	// Start the Gin server on port 8080
-	r.Run(":8080")
+	startServer(r)
+
+	// Block until a shutdown signal is received
+	<-stop
+	logrus.Infof("Shutting down...")
+}
+
+// to start service
+func startServer(router *gin.Engine) {
+	/*
+		DEFINITION : // Start the HTTP server to listen for incoming API requests on the registered routes.//
+	*/
+	server := &http.Server{
+		Addr:              constants.PORT,
+		Handler:           router,
+		WriteTimeout:      constants.TIMEOUT, // Use this constant constants.TIMEOUT
+		ReadHeaderTimeout: 2 * time.Second,
+	}
+	err := http2.ConfigureServer(server, nil)
+	if err != nil {
+		logrus.Errorf("Error while configuring http2", err)
+		return
+	}
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			logrus.Error("Server Closed", err)
+			return
+		}
+	}()
+	logrus.Errorf("Server listening at %s", constants.PORT)
+	Gracefullstop(server)
+
+}
+
+// to stop service gracefully
+func Gracefullstop(server *http.Server) {
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		logrus.Error(err)
+	}
+
 }
--- internal/utils/constants/constants.go
+++ internal/utils/constants/constants.go
@@ -1,6 +1,10 @@
 package constants
 
+import "time"
+
 const (
-	ProcessReceipts string = "/receipts/process"
-	GetPoints       string = "/receipts/points/:id"
+	ProcessReceipts string        = "/receipts/process"
+	GetPoints       string        = "/receipts/points/:id"
+	PORT            int           = 8080
+	TIMEOUT         time.Duration = 20 * time.Second
 )
