b5435a8628a119392910584af0c76c79da21e920
tarang <tarang@stackfusion.io>
2024-11-23 23:44:32 +0530

    implemented receipt store interface in the form of a hashmap based memorystore object

--- internal/repository/memory_store.go
+++ internal/repository/memory_store.go
@@ -6,12 +6,37 @@ import (
 	"sync"
 )
 
-// This is a simple struct that contains a map and a mutex to serve as the in memory store for us
+// MemoryStore implements the ReceiptStore interface using in-memory storage
 type MemoryStore struct {
 	mu       sync.Mutex
 	receipts map[string]models.Receipt
 }
 
-func NewMemoryStore() *MemoryStore {
-	return
+// NewMemoryStore creates a new MemoryStore instance that implements the ReceiptStore Interface
+func NewMemoryStore() ReceiptStore {
+	return &MemoryStore{
+		receipts: make(map[string]models.Receipt),
+	}
+}
+
+// AddReceipt adds a new receipt to the in-memory store and returns the generated ID
+func (s *MemoryStore) AddReceipt(receipt models.Receipt) string {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	id := uuid.New().String()
+	receipt.ID = id
+	s.receipts[id] = receipt
+	return id
+}
+
+// GetReceiptByID retrieves a receipt by its ID from the in-memory store
+func (s *MemoryStore) GetReceiptByID(id string) (models.Receipt, bool) {
+
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	receipt, exists := s.receipts[id]
+	if !exists {
+		return models.Receipt{}, false
+	}
+	return receipt, exists
 }
--- internal/repository/store.go
+++ internal/repository/store.go
@@ -1,1 +1,12 @@
 package repository
+
+import "github.com/TechnoDiktator/fetch-rewards-challange/internal/models"
+
+// Interface the defines the methods that the In memory Memory store will implement
+type ReceiptStore interface {
+	//Adds a new Receipt To the Implementor of this interface
+	AddReceipt(receipt models.Receipt) string
+
+	//Gets the Receipt By Id . The implementer will return the receipt object
+	GetReceiptByID(id string) (models.Receipt, bool)
+}
