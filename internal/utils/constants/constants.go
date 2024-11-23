1af1cae6c9977cdaaecc4b57eed4e47298412581
tarang <tarang@stackfusion.io>
2024-11-24 03:35:59 +0530

    gracefull stop for the servcer added

--- internal/utils/constants/constants.go
+++ internal/utils/constants/constants.go
@@ -5,6 +5,6 @@ import "time"
 const (
 	ProcessReceipts string        = "/receipts/process"
 	GetPoints       string        = "/receipts/points/:id"
-	PORT            int           = 8080
+	PORT            string        = "8080"
 	TIMEOUT         time.Duration = 20 * time.Second
 )
